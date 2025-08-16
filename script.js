6abac514cbd089db302773d675d0ef1d503ed068
Fatima Rahimi <rahimifatima432@gmail.com>
2025-08-16 15:19:02 +0430

    make javascript code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.querySelector("body").addEventListener("mousemove",
         eye.forEach(function(eye) {
         let x = eye.getBoundingClientRect().left + eye.clientWidth;
         let y = eye.getBoundingClientRect().right + eye.clientHeight;
-        let radian = Math.tana2(event.pageX -x, event.pageY -y);
+        let radian = Math.atan2(event.pageX - x, event.pageY -y);
         let rotate = radian * (180 / Math.PI) * -1 + 270;
         eye.style.transform = "rotate("+ rotate +"deg)";
         });
