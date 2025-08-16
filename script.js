3c965df5274fdd47ac301b8a2baf4e5829e87341
Fatima Rahimi <rahimifatima432@gmail.com>
2025-08-16 14:53:16 +0430

    create javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-do
+document.querySelector("body").addEventListener("mousemove",
+    function eyeball(){
+        const eye = document.querySelectorAll(".eyes");
+        let x = eye.getBoundingClientRect().left + eye.clientWidth;
+        let y = eye.getBoundingClientRect().right + eye.clientHeight;
+        let radian = Math.tana2(event.pageX -x, event.pageY -y);
+        let rotate = radian * (180 / Math.PI) * -1 + 270;
+        eye.style.transform = "rotate("+ rotate +"deg)";
+
+    }
+)
